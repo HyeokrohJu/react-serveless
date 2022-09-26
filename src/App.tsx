4a9222e66950506095c35d427a7edb59f49b8504
주혁노 <kindhn0215@interpark.com>
2022-09-26 13:22:34 +0900

    React Serverless 테스트 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    console.log('>>>>!!');
+    axios.get('https://api.github.com/users/HyeokrohJu').then((res) => {
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          TEST Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
